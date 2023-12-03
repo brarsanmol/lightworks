800a168b245228cb9083debc3b08b61bedf4ddf9
Anmol Brar <brarsanmol@protonmail.com>
2023-12-03 13:39:37 -0500

    feat: add disco flashlight
    - create basic config binding system.
    - create proof of concept patch for disco flashlight.

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 
 namespace LightWorks
@@ -11,12 +13,59 @@ namespace LightWorks
         
         private void Awake()
         {
+            this.AssignConfigValues();
+            
             this._harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            Logger.LogInfo("Loaded HarmonyLib!");
             this._harmony.PatchAll();
-            Logger.LogInfo("HarmonyLib is loaded!");
+            this.ListPatches();
             
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        /// <summary>
+        /// List each patch that HarmonyLib has applied.
+        /// This includes the class name, and the method name.
+        /// </summary>
+        private void ListPatches()
+        {
+            foreach (var method in this._harmony.GetPatchedMethods())
+            {
+                Logger.LogInfo($"Patched {method.DeclaringType.Name}.{method.Name}");
+            }
+        }
+
+        /// <summary>
+        /// Retrieve each configuration value and assign it the appropriate variable in the ConfigValues class.
+        /// </summary>
+        private void AssignConfigValues()
+        {
+            ConfigValues.DISCO_FLASHLIGHT_TOGGLED = this.GetConfigValue(
+                "DiscoFlashlight",
+                "Enabled",
+                true,
+                new ConfigDescription(
+                    "Whether the flashlight transitions through a rainbow of colours!",
+                    new AcceptableValueList<bool>(true, false)
+                )
+            ).Value;
+        }
+
+        /// <summary>
+        /// Retrieve a configuration value with a section and key, also allow specification of a description and default value.
+        /// </summary>
+        /// <param name="section">The section of the configuration file we are accessing.</param>
+        /// <param name="key">The key within the section to access.</param>
+        /// <param name="defaultValue">If the configuration entry does not exist, the default value to be specified.</param>
+        /// <param name="description">The purpose and usage of the configuration entry.</param>
+        /// <typeparam name="T">The type of the value in the ConfigEntry.</typeparam>
+        /// <returns>A ConfigEntry object of type T.</returns>
+        private ConfigEntry<T> GetConfigValue<T>(string section, string key, T defaultValue, ConfigDescription description)
+        {
+            var entry = Config.Bind(section, key, defaultValue, description);
+            Logger.LogInfo($"Retrieved ConfigValue {section}.{key} as {entry.Value}");
+            return entry;
         }
     }
 }
