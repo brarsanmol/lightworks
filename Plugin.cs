8b36dd8ecffb63f5911d1f22105d6288d7c3474f
Anmol Brar <brarsanmol@protonmail.com>
2023-12-03 03:32:10 -0500

    feat(Plugin.cs): initialize harmonylib

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,20 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace LightWorks
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
     public class Plugin : BaseUnityPlugin
     {
+        private Harmony _harmony;
+        
         private void Awake()
         {
+            this._harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            this._harmony.PatchAll();
+            Logger.LogInfo("HarmonyLib is loaded!");
+            
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
